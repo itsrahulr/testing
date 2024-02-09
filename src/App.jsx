dea809fae2ce85f822d5023c5153a531de068e6e
itsrahulr <therahul27r@gmail.com>
2024-02-09 20:12:22 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Lol from "./assets/Components/Lol";
+import Test from "./assets/Components/Test";
+
+function App() {
+  return (
+    <>
+      <Test />
+      <Lol />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/Components/Lol.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Lol = () => {
+  return (
+    <div className="h-screen w-full ">
+      <img src="/src/assets/images/p-1.jpg" alt="" />
+    </div>
+  );
+};
+
+export default Lol;
